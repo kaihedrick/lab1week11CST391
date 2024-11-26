0773eb45c6b755ab2d926356deb53850f54ff135
kaihedrick <kaihedrick@icloud.com>
2024-11-25 22:54:31 -0700

    First commit

---
+++ src/MyComponent.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './MyComponent.css'; // Import the CSS file for styling
+
+const MyComponent = () => {
+    return (
+        <div className="container">
+            <h1>Welcome to Our Colorful Website!</h1>
+            <p>Click the button below to explore our vibrant world:</p>
+            <button className="colorful-button">Explore Now</button>
+        </div>
+    );
+};
+
+export default MyComponent;
